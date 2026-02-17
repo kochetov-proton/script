e8b6196b21cfdf09af182660211dfe3f6e8a96f3
Oleg <jsonko@proton.me>
2026-02-17 15:39:23 +0300

    Update gitlab_install.sh

--- gitlab_install.sh
+++ gitlab_install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-sudo apt update​
-sudo apt install -y curl openssh-server ca-certificates tzdata perl postfix​
+sudo apt update
+sudo apt install -y curl openssh-server ca-certificates tzdata perl postfix
 
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
-sudo EXTERNAL_URL= "$(hostname -I | awk '{print $1}')" apt install gitlab-ce​​
+#  Установка с автоматическим определением IP (БЕЗ пробела после =)
+CURRENT_IP=$(hostname -I | awk '{print $1}')
+sudo EXTERNAL_URL="http://$CURRENT_IP" apt install gitlab-ce
 
-sudo cat /etc/gitlab/initial_root_password​
+echo "--------------------------------------------------------"
+echo "Установка завершена! Логин: root"
+echo "Ваш временный пароль находится ниже:"
+sudo cat /etc/gitlab/initial_root_password
