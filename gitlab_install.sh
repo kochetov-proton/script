33ab93ce52b1dc407097dd2756048abc190fb5f4
Oleg <jsonko@proton.me>
2026-02-17 15:41:11 +0300

    Update gitlab_install.sh

--- gitlab_install.sh
+++ gitlab_install.sh
@@ -5,7 +5,7 @@ sudo apt install -y curl openssh-server ca-certificates tzdata perl postfix
 
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
-#  Установка с автоматическим определением IP (БЕЗ пробела после =)
+#  Установка с автоматическим определением IP
 CURRENT_IP=$(hostname -I | awk '{print $1}')
 sudo EXTERNAL_URL="http://$CURRENT_IP" apt install gitlab-ce
 
